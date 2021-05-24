894f021d059c2787d3a7cfcd34b771d41ce044a2
Timothy Coleman <timhc22@gmail.com>
2021-05-24 19:23:24 +0100

    Add temporary url

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -2,12 +2,28 @@ import * as React from 'react';
 import {Button, StyleSheet, TextInput} from 'react-native';
 import {View} from '../components/Themed';
 import {useState} from "react";
+import axios from 'axios';
 
 export default function TabTwoScreen() {
   const [text1, setText1] = useState('1');
   const [text2, setText2] = useState('2');
   const [text3, setText3] = useState('3');
   const [text4, setText4] = useState('4');
+
+  const _onPressButton = () => {
+    axios.get('https://cafjm4ib00.execute-api.eu-west-2.amazonaws.com/live/')
+      .then(function (response: any) {
+        console.log(response)
+        const data = response.data;//
+        alert(data);
+        return;
+      })
+      .catch(function (error: any) {
+        alert('here22');
+        console.log(error);
+        return;
+      });
+  }
 
   return (
     <View style={styles.container}>
@@ -37,9 +53,7 @@ export default function TabTwoScreen() {
       />
 
       <Button
-        onPress={() => {
-          alert('You tapped the button!');
-        }}
+        onPress={_onPressButton}
         title="Update"
       />
       {/*<View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />*/}
