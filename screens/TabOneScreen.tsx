cb5d6be92d76fc39e4316554cf71791b2c443e11
Timothy Coleman <timhc22@gmail.com>
2021-05-26 20:00:58 +0100

    Switch tabs

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,23 +1,66 @@
 import * as React from 'react';
+import {Button, StyleSheet, Switch, TextInput} from 'react-native';
+import {View} from '../components/Themed';
 import {useState} from "react";
-import {StyleSheet, TextInput} from 'react-native';
-import {Text, View} from '../components/Themed';
+import axios from 'axios';
 
 export default function TabOneScreen() {
-  const [text, setText] = useState('');
+  const [text1, setText1] = useState("");
+  const [text2, setText2] = useState("");
+  const [text3, setText3] = useState("");
+  const [isEnabled, setIsEnabled] = useState(false);
+
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
+  const _onPressButton = () => {
+    axios.get('https://cafjm4ib00.execute-api.eu-west-2.amazonaws.com/live/')
+      .then(function (response: any) {
+        console.log(response)
+        const data = response.data;//
+        alert(data);
+        return;
+      })
+      .catch(function (error: any) {
+        alert('here22');
+        console.log(error);
+        return;
+      });
+  }
 
   return (
     <View style={styles.container}>
       <TextInput
-        style={{height: 40}}
-        placeholder="Type here to translate!"
-        onChangeText={text => setText(text)}
-        defaultValue={text}
+        style={inputStyles.input}
+        placeholder="Pairing"
+        onChangeText={text => setText1(text)}
+        defaultValue={text1}
       />
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <Text style={{padding: 10, fontSize: 42}}>
-        {text.split(' ').map((word) => word && '🍕').join(' ')}
-      </Text>
+      <TextInput
+        style={inputStyles.input}
+        placeholder="Low"
+        onChangeText={text => setText2(text)}
+        defaultValue={text2}
+      />
+      <TextInput
+        style={inputStyles.input}
+        placeholder="High"
+        onChangeText={text => setText3(text)}
+        defaultValue={text3}
+      />
+
+      <Switch
+        trackColor={{ false: "#767577", true: "#81b0ff" }}
+        thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+        ios_backgroundColor="#3e3e3e"
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+      />
+
+      <Button
+        onPress={_onPressButton}
+        title="Update"
+      />
+      {/*<View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />*/}
     </View>
   );
 }
@@ -36,5 +79,13 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+});
+
+const inputStyles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
   },
 });
--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,66 +1,23 @@
 import * as React from 'react';
-import {Button, StyleSheet, Switch, TextInput} from 'react-native';
-import {View} from '../components/Themed';
 import {useState} from "react";
-import axios from 'axios';
+import {StyleSheet, TextInput} from 'react-native';
+import {Text, View} from '../components/Themed';
 
 export default function TabTwoScreen() {
-  const [text1, setText1] = useState("");
-  const [text2, setText2] = useState("");
-  const [text3, setText3] = useState("");
-  const [isEnabled, setIsEnabled] = useState(false);
-
-  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
-
-  const _onPressButton = () => {
-    axios.get('https://cafjm4ib00.execute-api.eu-west-2.amazonaws.com/live/')
-      .then(function (response: any) {
-        console.log(response)
-        const data = response.data;//
-        alert(data);
-        return;
-      })
-      .catch(function (error: any) {
-        alert('here22');
-        console.log(error);
-        return;
-      });
-  }
+  const [text, setText] = useState('');
 
   return (
     <View style={styles.container}>
       <TextInput
-        style={inputStyles.input}
-        placeholder="Pairing"
-        onChangeText={text => setText1(text)}
-        defaultValue={text1}
+        style={{height: 40}}
+        placeholder="Type here to translate!"
+        onChangeText={text => setText(text)}
+        defaultValue={text}
       />
-      <TextInput
-        style={inputStyles.input}
-        placeholder="Low"
-        onChangeText={text => setText2(text)}
-        defaultValue={text2}
-      />
-      <TextInput
-        style={inputStyles.input}
-        placeholder="High"
-        onChangeText={text => setText3(text)}
-        defaultValue={text3}
-      />
-
-      <Switch
-        trackColor={{ false: "#767577", true: "#81b0ff" }}
-        thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
-        ios_backgroundColor="#3e3e3e"
-        onValueChange={toggleSwitch}
-        value={isEnabled}
-      />
-
-      <Button
-        onPress={_onPressButton}
-        title="Update"
-      />
-      {/*<View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />*/}
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <Text style={{padding: 10, fontSize: 42}}>
+        {text.split(' ').map((word) => word && '🍕').join(' ')}
+      </Text>
     </View>
   );
 }
@@ -79,13 +36,5 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
-  },
-});
-
-const inputStyles = StyleSheet.create({
-  input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
   },
 });
