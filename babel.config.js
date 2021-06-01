986a8a3110ca31d3c54f73aa3bccdf78336464ad
Timothy Coleman <timhc22@gmail.com>
2021-06-01 14:53:57 +0100

    Add notes

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,8 @@
 module.exports = function(api) {
+
+  // may need to run the following command when using with .env
+  // rm -rf node_modules/.cache/babel-loader/*
+  // https://github.com/goatandsheep/react-native-dotenv#caveats
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
